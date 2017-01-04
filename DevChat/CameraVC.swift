38a9e570f4bb90f56f2987b03edba6f937248c90
ricky04286 <reccles@Richards-MBP.att.net>
2017-01-03 20:52:23 -0600

    Project Created

---
+++ DevChat/CameraVC.swift
@@ -0,0 +1,19 @@
+//
+//  CameraVC.swift
+//  DevChat
+//
+//  Created by Richard Eccles on 1/3/17.
+//  Copyright © 2017 Richard Eccles. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
